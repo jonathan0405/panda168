2613308798c2babd301c7ad948ad13ea98685b9e
Jonathan Lu <jonathan850405@gmail.com>
2016-04-17 15:43:46 +0800

    to fit the indentation

--- database.py
+++ database.py
@@ -1,34 +1,34 @@
 from datetime import date
 from pony.orm import *
+from privatedata import *
 import os
 import csv
-from privatedata import *
 
 
 def searchfile():
-	top = 'price_data/'
-	for root, dirs, files in os.walk(top, topdown=False):
-		for name in files:
-			newcsvfile = (os.path.join(root, name))
-			company = name[:-9]
-			readfile(newcsvfile, company)
-			os.remove(newcsvfile)
+    top = 'price_data/'
+    for root, dirs, files in os.walk(top, topdown=False):
+        for name in files:
+            newcsvfile = (os.path.join(root, name))
+            company = name[:-9]
+            readfile(newcsvfile, company)
+            os.remove(newcsvfile)
 
 
 def readfile(newcsvfile, company):
-	print(newcsvfile)
-	with open(newcsvfile) as csvfile:
-		reader = csv.DictReader(csvfile)
-		for row in reader:
-			date = row['Date']
-			Open = row['Open']
-			high = row['High']
-			low = row['Low']
-			close = row['Close']
-			volume = row['Volume']
-			adjust = row['Adj Close']
-			add_price(company, date, Open, high, 
-				  low, close, volume, adjust)
+    print(newcsvfile)
+    with open(newcsvfile) as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            date = row['Date']
+            Open = row['Open']
+            high = row['High']
+            low = row['Low']
+            close = row['Close']
+            volume = row['Volume']
+            adjust = row['Adj Close']
+            add_price(company, date, Open, high,
+                       low, close, volume, adjust)
 
 
 def main():
@@ -51,7 +51,8 @@ def main():
 
 @db_session
 def add_price(company, date, open, high, low, close, volume, adjust):
-    Price(company=company, date=date, open=open, high=high, low=low, close=close, volume=volume,  adjust=adjust)
+    Price(company=company, date=date, open=open, high=high, 
+          low=low, close=close, volume=volume,  adjust=adjust)
 
 
 if __name__ == '__main__':
